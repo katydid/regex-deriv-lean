323fcd78a7d0b30d36269e4cfb26780770683e1f
Walter Schulze <awalterschulze@gmail.com>
2025-05-01 13:33:58 +0200

    remove Classical.Choice from NonEmptyList

--- Katydid/Std/NonEmptyList.lean
+++ Katydid/Std/NonEmptyList.lean
@@ -46,25 +46,18 @@ def merge [Ord α] (xs: NonEmptyList α) (ys: NonEmptyList α): NonEmptyList α 
   | Ordering.gt =>
     NonEmptyList.mk y1 (Lists.merge (x1::xs1) ys1)
 
+def eraseReps' [BEq α] (x: α) (xs: List α): List α :=
+  match xs with
+  | [] => []
+  | (x1::xs') =>
+    if x1 == x
+    then eraseReps' x xs'
+    else x1 :: eraseReps' x1 xs'
+
 def eraseReps [BEq α] (xs: NonEmptyList α): NonEmptyList α :=
   match xs with
-  | NonEmptyList.mk x1 xs1 =>
-  match xs1 with
-  | [] => xs
-  | (x2::xs2) =>
-    if x1 == x2
-    then NonEmptyList.eraseReps (NonEmptyList.mk x2 xs2)
-    else NonEmptyList.mk x1 (Lists.eraseReps xs1)
+  | NonEmptyList.mk x xs =>
+    NonEmptyList.mk x (eraseReps' x xs)
 
 def mergeReps [BEq α] [Ord α] (xs ys : NonEmptyList α): NonEmptyList α :=
-  match xs, ys with
-  | NonEmptyList.mk x1 xs1, NonEmptyList.mk y1 ys1 =>
-  match Ord.compare x1 y1 with
-  | Ordering.eq =>
-    match xs1 with
-    | [] =>
-      NonEmptyList.mk y1 ys1
-    | (x2::xs2) =>
-      NonEmptyList.mergeReps (NonEmptyList.mk x2 xs2) (NonEmptyList.mk y1 ys1)
-  | Ordering.lt => NonEmptyList.mk x1 (Lists.mergeReps xs1 (y1::ys1))
-  | Ordering.gt => NonEmptyList.mk y1 (Lists.mergeReps (x1::xs1) ys1)
+  eraseReps (merge xs ys)
