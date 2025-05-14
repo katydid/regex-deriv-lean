32318ebad519b8edd99c4b99c151e20c38402a4c
Walter Schulze <awalterschulze@gmail.com>
2025-05-14 10:33:50 +0200

    progress on proof for smartOr_is_correct_denote

--- Katydid/Regex/SmartRegexOr.lean
+++ Katydid/Regex/SmartRegexOr.lean
@@ -98,6 +98,11 @@ def insertOr [Ord α] [DecidableEq α] (x y: Regex α): Regex α :=
   | _ =>
     mkOr x y
 
+theorem Regex.simp_or_comm {α: Type} [Ord α] [DecidableEq α] (x y: Regex α):
+  denote (Regex.or x y) = denote (Regex.or y x) := by
+  simp only [denote]
+  apply Language.simp_or_comm
+
 theorem insertOr_is_correct_denote {α: Type} [Ord α] [DecidableEq α] (x y: Regex α):
   denote (Regex.or x y) = denote (insertOr x y) := by
   induction x with
@@ -124,16 +129,40 @@ def mergeOr {α: Type} [Ord α] [DecidableEq α] (x y: Regex α): Regex α :=
   | Regex.or x1 x2 =>
     match y with
     | Regex.or y1 y2 =>
-      match Ord.compare x1 y1 with
-      | Ordering.lt => Regex.or x1 (insertOr y1 (mergeOr x2 y2))
-      | Ordering.eq => mergeOr x2 y
-      | Ordering.gt => Regex.or y1 (insertOr x1 (mergeOr x2 y2))
+      if x1 = y1
+      then mergeOr x2 y
+      else if x1 < y1
+      then Regex.or x1 (insertOr y1 (mergeOr x2 y2))
+      else Regex.or y1 (insertOr x1 (mergeOr x2 y2))
     | _ => insertOr x y
   | _ => insertOr y x
 
 theorem mergeOr_is_correct_denote {α: Type} [Ord α] [DecidableEq α] (x y: Regex α):
   denote (Regex.or x y) = denote (mergeOr x y) := by
-  sorry
+  induction x with
+  | or x1 x2 ihx1 ihx2 =>
+    induction y with
+    | or y1 y2 ihy1 ihy2 =>
+      rw [mergeOr]
+      split_ifs
+      case pos h =>
+        rw [h]
+        rw [<- ihx2]
+        simp only [denote]
+        ac_rfl
+      case pos h =>
+        repeat rw [denote]
+        rw [<- insertOr_is_correct_denote]
+        repeat rw [<- denote]
+        sorry
+      case neg h =>
+        sorry
+    | _ =>
+      apply insertOr_is_correct_denote
+  | _ =>
+    simp [mergeOr]
+    rw [Regex.simp_or_comm]
+    apply insertOr_is_correct_denote
 
 theorem mergeOr_is_correct_sorted_no_dup
   {α: Type} [Ord α] [DecidableEq α] (x y: Regex α) (hx: OrIsSortedNoDup x) (hy: OrIsSortedNoDup y):
@@ -143,27 +172,110 @@ theorem mergeOr_is_correct_sorted_no_dup
 def smartOr {α: Type} [Ord α] [DecidableEq α] (x y: Regex α): Regex α :=
   match x with
   | Regex.emptyset => y
-  | Regex.star Regex.any => x
+  | Regex.star Regex.any => Regex.star Regex.any
   | Regex.or _ _ =>
     match y with
     | Regex.emptyset => x
-    | Regex.star Regex.any => y
+    | Regex.star Regex.any => Regex.star Regex.any
     | Regex.or _ _ =>
       mergeOr x y
     | _ =>
       insertOr x y
-  | _ =>
+  | x' =>
     match y with
-    | Regex.emptyset => x
-    | Regex.star Regex.any => y
-    | Regex.or _ _ =>
-      insertOr y x
-    | _ =>
-      Regex.or x y
+    | Regex.emptyset => x'
+    | Regex.star Regex.any => Regex.star Regex.any
+    | Regex.or y1 y2 =>
+      insertOr (Regex.or y1 y2) x'
+    | y' =>
+      Regex.or x' y'
+
+example {α: Type} [Ord α] [DecidableEq α]: (h: (@Regex.star α Regex.emptyset) = (Regex.star Regex.any)) -> False := by
+  intro h
+  simp at *
+
+theorem smartOr_is_correct_denote_star_l {α: Type} [Ord α] [DecidableEq α] (x1 y: Regex α):
+  denote (Regex.or (Regex.star x1) y) = denote (smartOr (Regex.star x1) y) := by
+  cases x1 with
+  | any =>
+    simp only [denote]
+    apply Language.simp_or_star_any_l_is_star_any
+  | _ =>
+    unfold smartOr
+    simp only
+    split
+    · simp only [denote]
+      apply Language.simp_or_emptyset_r_is_l
+    · simp only [denote]
+      apply Language.simp_or_star_any_r_is_star_any
+    · rename_i y1 y2
+      rw [<- insertOr_is_correct_denote]
+      simp only [denote]
+      ac_rfl
+    · rfl
+
+theorem smartOr_is_correct_denote_star_r {α: Type} [Ord α] [DecidableEq α] (x y1: Regex α):
+  denote (Regex.or x (Regex.star y1)) = denote (smartOr x (Regex.star y1)) := by
+  cases y1 with
+  | any =>
+    unfold smartOr
+    simp only [denote]
+    rw [Language.simp_or_star_any_r_is_star_any]
+    split <;> simp only [denote]
+  | _ =>
+    unfold smartOr
+    simp only
+    split
+    · simp only [denote]
+      apply Language.simp_or_emptyset_l_is_r
+    · simp only [denote]
+      apply Language.simp_or_star_any_l_is_star_any
+    · rename_i y1 y2
+      rw [<- insertOr_is_correct_denote]
+    · rfl
 
 theorem smartOr_is_correct_denote {α: Type} [Ord α] [DecidableEq α] (x y: Regex α):
   denote (Regex.or x y) = denote (smartOr x y) := by
-  sorry
+  induction x with
+  | emptyset =>
+    rw [smartOr]
+    simp only [denote]
+    ac_rfl
+  | star x1 =>
+    apply smartOr_is_correct_denote_star_l
+  | or x1 x2 ihx1 ihx2 =>
+    induction y with
+    | emptyset =>
+      rw [smartOr]
+      simp only [denote]
+      ac_rfl
+    | star _ =>
+      apply smartOr_is_correct_denote_star_r
+    | or y1 y2 ihy1 ihy2 =>
+      unfold smartOr
+      simp only
+      rw [<- mergeOr_is_correct_denote]
+    | _ =>
+      unfold smartOr
+      simp only
+      rw [<- insertOr_is_correct_denote]
+  | _ =>
+    induction y with
+    | emptyset =>
+      rw [smartOr] <;> (intros ; try contradiction)
+      · simp only [denote]
+        ac_rfl
+    | star _ =>
+      apply smartOr_is_correct_denote_star_r
+    | or y1 y1 ihy1 ihy2 =>
+      unfold smartOr
+      simp only
+      rw [<- insertOr_is_correct_denote]
+      simp only [denote]
+      ac_rfl
+    | _ =>
+      rw [smartOr] <;> simp only [reduceCtorEq, imp_self, implies_true]
+
 
 theorem smartOr_is_correct_sorted_no_dup
   {α: Type} [Ord α] [DecidableEq α] (x y: Regex α) (hx: OrIsSortedNoDup x) (hy: OrIsSortedNoDup y):
